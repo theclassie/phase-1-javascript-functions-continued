f1ab63bdccb17fe710f905d54cf8eb76d21c3327
Tristyn Broadhead <trisclassie@gmail.com>
2022-11-22 14:58:50 -0700

    commit tree cf9651d21a4cbe2a63e948c4b769c46b54938fcd

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun()
+
+const mondayWork = function(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork()
+
+function wrapAdjective(em = '*', adj){
+    let specialSentence = function(adj = 'special'){
+        return `You are ${em}${adj}${em}!`
+    }
+    return specialSentence;
+}
